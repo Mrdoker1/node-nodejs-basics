6c8982df304a7b178aafbd30a97d588149e2f95e
viachaskul <fixrapdoK@gmail.com>
2022-11-30 20:51:44 +0300

    feat: add resolved tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import fs from 'fs'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const src = `${__dirname}/files`;
+const dest = `${__dirname}/files_copy`;
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        fs.stat(dest, function(err, stat) {
+            if (err == null) {
+                throw new Error('FS operation failed');
+            } else {
+                fs.cp(src, dest, {recursive: true}, function (err, data) {});
+                console.log('success!')
+            }
+          });
+    } catch (err) {
+    console.error(err)
+    }
 };
 
 copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,31 @@
+import fs from 'fs'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filename = `${__dirname}/files/fresh.txt`;
+const text = 'I am fresh and young';
+
+const writeFile = () => {
+    fs.writeFile(filename, text, { flag: `a` }, err => {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+const createFile = () => {
+    fs.stat(filename, function(err, stat) {
+        if (err == null) {
+            throw new Error('FS operation failed');
+        } else {
+            writeFile();
+        }
+      });
+}
+
 const create = async () => {
-    // Write your code here 
+    createFile();
 };
 
 await create();
